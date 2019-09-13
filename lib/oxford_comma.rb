99f624bdbc055ad9c6ebeec8e02a362c3d07b0e6
Daniel Lechok <dclechok@gmail.com>
2019-09-13 16:19:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -16,7 +16,7 @@ def oxford_comma(array)
   if array.size >= 4
 
     return array[0..-2].join(", ") + ", and " + array[-1]
-=======
+
     return array[0..-2].join(", ") + " and " + array[-1]
 
   end
